91f9ecec6cc1dac3851bd918c917902dc9389c36
Håkan Råberg <hraberg@yahoo.se>
2016-04-01 06:50:47 +0100

    Sanity checking config on a 25 snapshot.

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 ;;; Commentary:
 
 ;; This is my portable .emacs.d which uses use-package.
-;; Tested with Emacs 24.3.1 and 24.5.1 on Linux.
+;; Tested with Emacs 24.3.1, 24.5.1 and 25.1.50.2 on Linux.
 
 ;;; Code:
 
